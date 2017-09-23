26a33590fa56624e364a6f56c21cb50207f1b883
richie.buckingham <richie.buckingham@gmail.com>
2017-09-23 16:58:35 +1000

    deleted unneccesary files

---
+++ src/app/events-app/events-app.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-events-app',
+  templateUrl: './events-app.component.html'
+})
+export class EventsAppComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
