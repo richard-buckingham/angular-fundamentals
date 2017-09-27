543d417a1d525693d1f9ad7e7f9681a44ca99de0
richie.buckingham <richie.buckingham@gmail.com>
2017-09-27 22:33:21 +1000

    lazy load the user module

---
+++ src/app/user/user.routes.ts
@@ -0,0 +1,8 @@
+import {Routes} from '@angular/router';
+
+import { ProfileComponent } from './profile/profile.component';
+
+// user/profile
+export const userRoutes: Routes = [
+    {path: 'profile', component: ProfileComponent}
+];
