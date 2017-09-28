039f17be38351b682782f2fb5fefbb8673e156c9
richie.buckingham <richie.buckingham@gmail.com>
2017-09-28 14:33:02 +1000

    creating a template driven form

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { userRoutes } from './user.routes';
 import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     RouterModule.forChild(userRoutes)
   ],
-  declarations: [ProfileComponent]
+  declarations: [ProfileComponent, LoginComponent]
 })
 export class UserModule { }
--- src/app/user/user.routes.ts
+++ src/app/user/user.routes.ts
@@ -1,8 +1,10 @@
 import {Routes} from '@angular/router';
 
 import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './login/login.component';
 
 // user/profile
 export const userRoutes: Routes = [
-    {path: 'profile', component: ProfileComponent}
+    {path: 'profile', component: ProfileComponent},
+    {path: 'login', component: LoginComponent}
 ];
